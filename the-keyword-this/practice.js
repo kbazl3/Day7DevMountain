dbf0569db856d4b1e47209d08477140b957619b6
Kyle Barlow <kbazlo@yahoo.com>
2016-02-09 13:19:13 -0700

    changed git commit email, testing if githubwill show contributions now

--- the-keyword-this/practice.js
+++ the-keyword-this/practice.js
@@ -2,6 +2,7 @@
   // 1) What is the purpose of the 'this keyword'?
 
       //Answer
+      //It is shorthand for indicating the object name
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
@@ -21,17 +22,34 @@
 //Create an object called user which has the following properties.
   //username --> which is a string
   //email --> which is a string
-  //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
+  //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use 
+  //the 'this' keyword*
 
     //Code Here
+    var user = {
+      username: "kbarlow",
+      email: "kb@gmail.com",
+      getUsername: function() {
+        return this.username;
+      }
+    };
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
-
+user.getUsername();
 
 //Next Problem
 
 
 // Write the function definitions which will make the following function invocations function properly.
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    var that = this;
+    this.moveCar: function {
+      return that.moveCar + 10;
+    }
+};
 
   //Function Invocations Here
 
@@ -41,7 +59,9 @@ var mustang = new Car('Ford', 'Mustang', 2013);
 prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
-//Hint, you'll need to write a moveCar function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
+//Hint, you'll need to write a moveCar function which is added to every object that is being returned from the Car function. 
+//You'll also need to use the 'this' keyword properly in order to make sure you're 
+//invoking moveCar on the right object (prius vs mustang).
 
 
 
